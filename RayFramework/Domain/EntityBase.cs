cb768f8b7a57dbe31818f80d169daef9745eab98
liekiss <289294189@qq.com>
2015-06-16 15:29:58 +0800

    Commit

---
+++ RayFramework/Domain/EntityBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Domain
+{
+    public abstract class EntityBase<TKey>
+    {
+        protected EntityBase(TKey id)
+        {
+            this.Id = id;
+        }
+
+        [Required]
+        public TKey Id { get; private set; }
+    }
+}
