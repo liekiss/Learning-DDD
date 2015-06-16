40ae455faab30abc642f7ce4daebd10a86f7a4e1
liekiss <289294189@qq.com>
2015-06-16 18:03:00 +0800

    EntityBase commit

--- RayFramework/Domain/EntityBase.cs
+++ RayFramework/Domain/EntityBase.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 
 namespace Domain
@@ -9,10 +7,56 @@ namespace Domain
     {
         protected EntityBase(TKey id)
         {
-            this.Id = id;
+            Id = id;
         }
 
         [Required]
         public TKey Id { get; private set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            if (!(obj is EntityBase<TKey>))
+                return false;
+            return this == (EntityBase<TKey>)obj;
+        }
+
+        /// <summary>
+        /// 根据实体标识Id获取hash
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        /// <summary>
+        /// 相等比较
+        /// </summary>
+        /// <param name="entity1">领域实体1</param>
+        /// <param name="entity2">领域实体2</param>
+        public static bool operator ==(EntityBase<TKey> entity1, EntityBase<TKey> entity2)
+        {
+            if ((object)entity1 == null && (object)entity2 == null)
+                return true;
+            if ((object)entity1 == null || (object)entity2 == null)
+                return false;
+            if (entity1.Id == null)
+                return false;
+            if (entity1.Id.Equals(default(TKey)))
+                return false;
+            return entity1.Id.Equals(entity2.Id);
+        }
+
+        /// <summary>
+        /// 不相等比较
+        /// </summary>
+        /// <param name="entity1">领域实体1</param>
+        /// <param name="entity2">领域实体2</param>
+        public static bool operator !=(EntityBase<TKey> entity1, EntityBase<TKey> entity2)
+        {
+            return !(entity1 == entity2);
+        }
     }
 }
---
+++ RayFramework/Domain/EntityBaseGuid.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public abstract class EntityBaseGuid : EntityBase<Guid>
+    {
+        protected EntityBaseGuid(Guid id)
+            : base(id)
+        {
+
+        }
+    }
+}
